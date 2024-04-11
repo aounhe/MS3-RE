60aa71913c8df43e2c12e1060d518a87eb273b8c
Houssem Aoun <aounhe@hotmail.com>
2024-04-11 21:58:55 +0000

    Add manage categories template

--- app.py
+++ app.py
@@ -147,6 +147,11 @@ def delete_film(film_id):
     return redirect(url_for("get_films"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
